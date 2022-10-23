0c2105049d7ee537044caaa8ba91d2e83b7d9144
ilimsk <113999486+ilimsk@users.noreply.github.com>
2022-10-23 13:35:17 +0700

    Update deposit.py
    add remark

--- deposit.py
+++ deposit.py
@@ -2,8 +2,8 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = float (input ("Введите сумму: "))
 deposit = []
 for key in per_cent:
-    print (key, (per_cent[key]*money)/100)
+    #print (key, (per_cent[key]*money)/100)
     deposit.append ((per_cent[key]*money)/100)
-print (deposit)
+#print (deposit)
 deposit.sort()
-print("Размер наибольшего дохода:", deposit[-1])
+print("Максимальная сумма, которую вы можете заработать —", deposit[-1])
